44cba43412851574294539631619e8ec90bb4b5f
Zuzanna <jakzuzia@gmail.com>
2021-08-19 13:06:04 +0200

    cokolwiek

---
+++ testv2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace testv2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pierwsza liczba");
+            string liczba1;
+            liczba1 = Console.ReadLine();
+            Console.WriteLine("Druga liczba");
+            string liczba2;
+            liczba2 = Console.ReadLine();
+            Console.WriteLine("Twoje liczby to:" + liczba1 + liczba2);
+
+        }
+    }
+}
