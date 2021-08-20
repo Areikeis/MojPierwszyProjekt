fa6ff164a2eaf035e41c7a68196fc0c4e9c518c3
Zuzanna <jakzuzia@gmail.com>
2021-08-20 13:56:38 +0200

    porownywanie liczb

--- testv2/Program.cs
+++ testv2/Program.cs
@@ -7,13 +7,28 @@ namespace testv2
         static void Main(string[] args)
         {
             Console.WriteLine("Pierwsza liczba");
-            string liczba1;
-            liczba1 = Console.ReadLine();
-            Console.WriteLine("Druga liczba");
-            string liczba2;
-            liczba2 = Console.ReadLine();
-            Console.WriteLine("Twoje liczby to:" + liczba1 + liczba2);
+            int liczba1 = Convert.ToInt32(Console.ReadLine());
 
+            Console.WriteLine("Druga liczba");
+            int liczba2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Twoje liczby to: " + $"{liczba1} {liczba2}");
+
+            if (liczba1 > liczba2)
+            {
+                int roznica = liczba1 - liczba2;
+                Console.WriteLine("Liczba pierwsza jest większa od drugiej o " + roznica);
+            }
+            else if (liczba1 < liczba2)
+            {
+                int roznica = liczba2 - liczba1;
+                Console.WriteLine("Liczba druga jest większa od pierwszej o " + roznica);
+            }
+            else if (liczba1 == liczba2)
+            {
+                Console.WriteLine("Obie liczby są równe");
+            }
+            
         }
     }
 }
